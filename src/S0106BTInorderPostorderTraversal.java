4168cb20ff7f9d3638fb7faf044c3a348eb98e0c
camel <camel.young@gmail.com>
2018-07-01 21:26:51 -0700

    7/1

---
+++ src/S0106BTInorderPostorderTraversal.java
@@ -0,0 +1,44 @@
+public class S0106BTInorderPostorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder == null || inorder.length == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder[postorder.length-1]);
+        if (postorder.length == 1) {
+            return root;
+        }
+
+        int count = 0;
+        while (inorder[count] != root.val) {
+            count++;
+        }
+        int[] leftIn = new int[count];
+        int[] leftPost = new int[count];
+        for (int i = 0; i < count; i++) {
+            leftIn[i] = inorder[i];
+            leftPost[i] = postorder[i];
+        }
+        root.left = buildTree(leftIn, leftPost);
+
+        int rightCount = inorder.length-count-1;
+        int[] rightIn = new int[rightCount];
+        int[] rightPost = new int[rightCount];
+        for (int i = 0; i < rightCount; i++) {
+            rightIn[i] = inorder[i+count+1];
+            rightPost[i] = postorder[count+i];
+        }
+        root.right = buildTree(rightIn, rightPost);
+        return root;
+    }
+}
