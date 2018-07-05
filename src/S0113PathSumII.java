f66ef40670bf6934d59f185bbfc84f47bca73488
camel <camel.young@gmail.com>
2018-07-04 21:43:21 -0700

    7/4

---
+++ src/S0113PathSumII.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class S0113PathSumII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                List<Integer> a = new ArrayList<>();
+                a.add(root.val);
+                res.add(a);
+            }
+            return res;
+        }
+
+        int val = sum - root.val;
+        if (root.left != null) {
+            for (List<Integer> path : pathSum(root.left, val)) {
+                path.add(0, root.val);
+                res.add(path);
+            }
+        }
+
+        if (root.right != null) {
+            for (List<Integer> path : pathSum(root.right, val)) {
+                path.add(0, root.val);
+                res.add(path);
+            }
+        }
+        return res;
+    }
+}
