23660409592a9f3add5d20ff44f07c67d2442fda
camel <camel.young@gmail.com>
2018-07-18 15:12:04 -0700

    7/18

---
+++ src/S0144BTPreorderTraversal.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class S0144BTPreorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        List<TreeNode> stack = new ArrayList<>();
+        stack.add(root);
+        do {
+            TreeNode node = stack.remove(stack.size()-1);
+            while (node != null) {
+                res.add(node.val);
+                if (node.right != null) {
+                    stack.add(node.right);
+                }
+                // left
+                node = node.left;
+            }
+        } while (!stack.isEmpty());
+        return res;
+    }
+}
