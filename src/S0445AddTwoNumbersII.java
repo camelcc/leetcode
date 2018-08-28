e10bc568cf3694f03ad65b85ea0c6e20fb86ba5d
camel <camel.young@gmail.com>
2018-08-28 11:10:31 -0700

    8/28

---
+++ src/S0445AddTwoNumbersII.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+public class S0445AddTwoNumbersII {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<ListNode> sum = new Stack<>();
+        Stack<ListNode> l1Stack = new Stack<>();
+        while (l1 != null) {
+            l1Stack.push(l1);
+            l1 = l1.next;
+        }
+        Stack<ListNode> l2Stack = new Stack<>();
+        while (l2 != null) {
+            l2Stack.push(l2);
+            l2 = l2.next;
+        }
+
+        int c = 0;
+        while (!l1Stack.isEmpty() || !l2Stack.isEmpty()) {
+            int l1Val = l1Stack.isEmpty() ? 0 : l1Stack.pop().val;
+            int l2Val = l2Stack.isEmpty() ? 0 : l2Stack.pop().val;
+
+            int s = c + l1Val + l2Val;
+            c = s / 10;
+            sum.push(new ListNode(s%10));
+        }
+        if (c != 0) {
+            sum.push(new ListNode(c));
+        }
+
+        ListNode res = sum.pop();
+        ListNode node = res;
+        while (!sum.isEmpty()) {
+            node.next = sum.pop();
+            node = node.next;
+        }
+        return res;
+    }
+}
---
+++ src/S0449SerializeDeserializeBST.java
@@ -0,0 +1,77 @@
+
+public class S0449SerializeDeserializeBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class Codec {
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+            if (root == null) {
+                return "n";
+            }
+
+            String res = String.valueOf(root.val) + ",";
+
+            String leftTree = serialize(root.left);
+            assert !leftTree.isEmpty();
+            int commaCount = 0;
+            int commaIndex = -1;
+            while (true) {
+                commaIndex = leftTree.indexOf(",", commaIndex);
+                if (commaIndex != -1) {
+                    commaCount++;
+                    commaIndex++;
+                } else {
+                    break;
+                }
+            }
+            leftTree = String.valueOf(commaCount+1) + "," + leftTree;
+            res = res + leftTree;
+
+            String rightTree = serialize(root.right);
+            res = res + "," + rightTree;
+            return res;
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            assert !data.isEmpty();
+            if (data.equals("n")) {
+                return null;
+            }
+
+            int split = data.indexOf(",");
+            String rootStr = data.substring(0, split);
+            assert !rootStr.isEmpty();
+            TreeNode root = new TreeNode(Integer.valueOf(rootStr));
+            String remainsStr = data.substring(split+1);
+
+            split = remainsStr.indexOf(",");
+            String leftCountStr = remainsStr.substring(0, split);
+            assert !leftCountStr.isEmpty();
+            int leftCount = Integer.valueOf(leftCountStr);
+            assert leftCount > 0;
+            remainsStr = remainsStr.substring(split+1);
+            int leftTreeIndex = -1;
+            while (leftCount > 0) {
+                leftTreeIndex++;
+                leftTreeIndex = remainsStr.indexOf(",", leftTreeIndex);
+                leftCount--;
+            }
+            String leftTreeStr = remainsStr.substring(0, leftTreeIndex);
+            root.left = deserialize(leftTreeStr);
+
+            remainsStr = remainsStr.substring(leftTreeIndex+1);
+            root.right = deserialize(remainsStr);
+
+            return root;
+        }
+    }
+}
