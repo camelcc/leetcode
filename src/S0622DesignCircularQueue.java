2c2f67e12ef50542c51c72aec14490277878990c
camel <camel.young@gmail.com>
2018-09-07 10:23:53 -0700

    9/7

--- src/S0622DesignCircularQueue.java
+++ src/S0622DesignCircularQueue.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
-
 public class S0622DesignCircularQueue {
+    class MyCircularQueue {
+        private int[] queue;
+        private int N;
+        private int head;
+        private int tail;
+
+        /** Initialize your data structure here. Set the size of the queue to be k. */
+        public MyCircularQueue(int k) {
+            this.N = k+1;
+            queue = new int[k+1];
+            head = 0;
+            tail = 0;
+        }
+
+        /** Insert an element into the circular queue. Return true if the operation is successful. */
+        public boolean enQueue(int value) {
+            if (isFull()) {
+                return false;
+            }
+
+            queue[tail] = value;
+            tail = (tail + 1) % N;
+            return true;
+        }
+
+        /** Delete an element from the circular queue. Return true if the operation is successful. */
+        public boolean deQueue() {
+            if (isEmpty()) {
+                return false;
+            }
+
+            head = (head + 1) % N;
+            return true;
+        }
+
+        /** Get the front item from the queue. */
+        public int Front() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[head];
+        }
+
+        /** Get the last item from the queue. */
+        public int Rear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[(tail + N - 1) % N];
+        }
+
+        /** Checks whether the circular queue is empty or not. */
+        public boolean isEmpty() {
+            return head == tail;
+        }
+
+        /** Checks whether the circular queue is full or not. */
+        public boolean isFull() {
+            return (tail + 1) % N == head;
+        }
+    }
 }
--- src/S0641DesignCircularDeque.java
+++ src/S0641DesignCircularDeque.java
@@ -1,4 +1,86 @@
-package PACKAGE_NAME;
-
 public class S0641DesignCircularDeque {
+    class MyCircularDeque {
+        private int N;
+        private int[] queue;
+        private int head;
+        private int tail;
+
+        /** Initialize your data structure here. Set the size of the deque to be k. */
+        public MyCircularDeque(int k) {
+            N = k + 1;
+            queue = new int[k + 1];
+            head = 0;
+            tail = 0;
+        }
+
+        /** Adds an item at the front of Deque. Return true if the operation is successful. */
+        public boolean insertFront(int value) {
+            if (isFull()) {
+                return false;
+            }
+
+            head = (head + N - 1) % N;
+            queue[head] = value;
+            return true;
+        }
+
+        /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+        public boolean insertLast(int value) {
+            if (isFull()) {
+                return false;
+            }
+
+            queue[tail] = value;
+            tail = (tail + 1) % N;
+            return true;
+        }
+
+        /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+        public boolean deleteFront() {
+            if (isEmpty()) {
+                return false;
+            }
+
+            head = (head + 1) % N;
+            return true;
+        }
+
+        /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+        public boolean deleteLast() {
+            if (isEmpty()) {
+                return false;
+            }
+
+            tail = (tail + N - 1 ) % N;
+            return true;
+        }
+
+        /** Get the front item from the deque. */
+        public int getFront() {
+            if (isEmpty()) {
+                return  -1;
+            }
+
+            return queue[head];
+        }
+
+        /** Get the last item from the deque. */
+        public int getRear() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return queue[(tail - 1 + N) % N];
+        }
+
+        /** Checks whether the circular deque is empty or not. */
+        public boolean isEmpty() {
+            return head == tail;
+        }
+
+        /** Checks whether the circular deque is full or not. */
+        public boolean isFull() {
+            return (tail + 1) % N == head;
+        }
+    }
 }
