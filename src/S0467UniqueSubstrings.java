d3c4e155accb8b149b0990ae521c112e7d139196
camel <camel.young@gmail.com>
2018-09-26 20:25:20 -0700

    9/26

--- src/S0467UniqueSubstrings.java
+++ src/S0467UniqueSubstrings.java
@@ -1,51 +1,22 @@
-import java.util.HashSet;
-
-//TODO: DP solution
 public class S0467UniqueSubstrings {
     public int findSubstringInWraproundString(String p) {
-        if (p.isEmpty()) {
-            return 0;
-        }
-
-        HashSet<String> segments = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
-        sb.append(p.charAt(0));
-        for (int i = 1; i < p.length(); i++) {
-            char last = sb.charAt(sb.length() - 1);
-            char current = p.charAt(i);
-            if (last == 'z' && current == 'a' || current - last == 1) {
-                sb.append(current);
+        int count[] = new int[26];
+        int continous = 0;
+        for (int i = 0; i < p.length(); i++) {
+            if (i > 0 && ((p.charAt(i) - p.charAt(i-1)) == 1 || p.charAt(i-1) - p.charAt(i) == 25)) {
+                continous++;
             } else {
-                String seg = sb.toString();
-                String match = null;
-                for (String s : segments) {
-                    if (s.contains(seg) || seg.contains(s)) {
-                        match = s;
-                        break;
-                    }
-                }
-
-                if (match != null) {
-                    assert !match.isEmpty();
-                    if (seg.contains(match)) {
-                        segments.remove(match);
-                        segments.add(seg);
-                    }
-                } else {
-                    segments.add(sb.toString());
-                }
-                sb = new StringBuilder();
-                sb.append(current);
+                continous = 1;
             }
-        }
-        if (sb.length() > 0) {
-            segments.add(sb.toString());
+
+            char c = p.charAt(i);
+            count[c - 'a'] = Math.max(count[c - 'a'], continous);
         }
 
-        int cnt = 0;
-        for (String s : segments) {
-            cnt += (1 + s.length()) * s.length() / 2;
+        int res = 0;
+        for (int i = 0 ; i < count.length; i++) {
+            res += count[i];
         }
-        return cnt;
+        return res;
     }
 }
