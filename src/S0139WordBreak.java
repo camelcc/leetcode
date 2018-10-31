db969aa9abdb773c61cc6e49035e069f18bea743
camel <camel.young@gmail.com>
2018-10-31 10:32:55 -0700

    10/31

---
+++ src/S0139WordBreak.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.List;
+
+public class S0139WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> dict = new HashSet<>(wordDict);
+        boolean[] res = new boolean[s.length()+1];
+        res[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (res[j] && dict.contains(s.substring(j, i))) {
+                    res[i] = true;
+                    break;
+                }
+            }
+        }
+        return res[s.length()];
+    }
+}
