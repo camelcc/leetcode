69be99df623f0b8d7b342294e3a4f3f6ae09cf0a
camel <camel.young@gmail.com>
2018-11-24 16:54:04 -0800

    11/24

---
+++ src/S0328OddEvenLinkedList.java
@@ -0,0 +1,52 @@
+public class S0328OddEvenLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode oddHead = null;
+        ListNode oddTail = null;
+        ListNode evenHead = null;
+        ListNode evenTail = null;
+
+        ListNode n = head;
+        while (n != null) {
+            ListNode odd = n;
+            ListNode even = n.next;
+            if (oddHead == null) {
+                oddHead = odd;
+                oddTail = odd;
+            } else {
+                oddTail.next = odd;
+                oddTail = oddTail.next;
+            }
+            if (evenHead == null) {
+                evenHead = even;
+                evenTail = even;
+            } else {
+                evenTail.next = even;
+                evenTail = evenTail.next;
+            }
+            if (even != null) {
+                n = even.next;
+            } else {
+                break;
+            }
+        }
+        oddTail.next = evenHead;
+        if (evenTail != null) {
+            evenTail.next = null;
+        }
+
+        return head;
+    }
+}
