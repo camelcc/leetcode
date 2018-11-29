fbf49fe12db659b6239c4d600611bb6932cb1c89
camel <camel.young@gmail.com>
2018-11-29 13:01:09 -0800

    11/29

---
+++ src/S0435NonoverlappingIntervals.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class S0435NonoverlappingIntervals {
+    public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if (intervals.length <= 1) {
+            return 0;
+        }
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.end - o2.end;
+            }
+        });
+
+        int end = intervals[0].end;
+        int count = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start >= end) {
+                count++;
+                end = intervals[i].end;
+            }
+        }
+
+        return intervals.length - count;
+    }
+}
