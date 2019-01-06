c3776558198863954b0d5db5320c4a0cd9f5caa2
camel <camel.young@gmail.com>
2019-01-06 12:59:12 -0800

    1/6

---
+++ src/S0623AddOneRowTree.java
@@ -0,0 +1,38 @@
+public class S0623AddOneRowTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) {
+            TreeNode res = new TreeNode(v);
+            res.left = root;
+            return res;
+        }
+
+        if (root == null) {
+            return null;
+        }
+
+        if (d == 2) {
+            TreeNode l = root.left;
+            TreeNode r = root.right;
+            root.left = new TreeNode(v);
+            root.left.left = l;
+            root.right = new TreeNode(v);
+            root.right.right = r;
+            return root;
+        }
+
+        root.left = addOneRow(root.left, v, d-1);
+        root.right = addOneRow(root.right, v, d-1);
+
+        return root;
+    }
+}
