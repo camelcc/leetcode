8bd44cb08a3b181ca0b37ab35b7962bf1abe09c7
camel <camel.young@gmail.com>
2019-01-16 10:29:34 -0800

    1/16

---
+++ src/S0654MaximumBinaryTree.java
@@ -0,0 +1,34 @@
+public class S0654MaximumBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return build(nums, 0, nums.length-1);
+    }
+
+    private TreeNode build(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new TreeNode(nums[start]);
+        }
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[i] > nums[max]) {
+                max = i;
+            }
+        }
+        TreeNode root = new TreeNode(nums[max]);
+        root.left = build(nums, start, max-1);
+        root.right = build(nums, max+1, end);
+        return root;
+    }
+}
