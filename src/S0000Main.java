385e4f85256676834a79b71e46681c8441bfe786
camel <camel.young@gmail.com>
2019-03-24 21:49:40 -0700

    3/24

--- src/S0000Main.java
+++ src/S0000Main.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class S0000Main {
     public static void main(String[] args) {
-        S0835ImageOverlap overlap = new S0835ImageOverlap();
-        StdOut.println(overlap.largestOverlap(new int[][]{{1, 1, 0}, {0, 1, 0}, {0, 1, 0}}, new int[][]{{0, 0, 0}, {0, 1, 1}, {0, 0, 1}}));
+        S0837New21Game game = new S0837New21Game();
+        StdOut.println(game.new21Game(21 ,17, 10));
     }
 }
