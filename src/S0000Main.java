6c29b15f590b205a13b04055ed86dd35658a1d50
camel <camel.young@gmail.com>
2019-04-28 16:33:13 -0700

    4/28

--- src/S0000Main.java
+++ src/S0000Main.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class S0000Main {
     public static void main(String[] args) {
-        S0087ScrambleString scrambleString = new S0087ScrambleString();
-        StdOut.println(scrambleString.isScramble("abcde", "caebd"));
+        S0097InterleavingString string = new S0097InterleavingString();
+        StdOut.println(string.isInterleave("aabc", "abad", "aabcabad"));
     }
 }
