323dd17bd7093eea297a46e373ecdfe3927d1162
camel <camel.young@gmail.com>
2019-05-02 16:00:29 -0700

    5/2

--- src/S0000Main.java
+++ src/S0000Main.java
@@ -1,12 +1,8 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class S0000Main {
     public static void main(String[] args) {
-        S0146LRUCache.LRUCache cache = new S0146LRUCache.LRUCache(2);
-        cache.put(2, 1);
-        cache.put(1, 1);
-        cache.put(2, 3);
-        cache.put(4, 1);
-
-        cache.get(1);
-        cache.get(2);
+        S0188BestTimeBuySellStockIV stock = new S0188BestTimeBuySellStockIV();
+        StdOut.println(stock.maxProfit(2, new int[]{2,1,4,5,2,9,7}));
     }
 }
