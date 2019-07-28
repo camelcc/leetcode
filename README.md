1298610f04415cbda0bb4d11d584e933732b92f8
camel <camel.young@gmail.com>
2019-07-27 21:16:52 -0700

    7/27

--- README.md
+++ README.md
@@ -173,6 +173,7 @@ This repo is leetcode solutions in Java. Also include `algs4.jar` library for te
 [W] 0172. Factorial Trailing Zeroes
 [W] 0173. Binary Search Tree Iterator
 [L] 0174. Dungeon Game
+[W] 0175. Combine Two Tables
 [B] 0179. Largest Number
 [B] 0187. Repeated DNA Sequences
 [B] 0188. Best Time to Buy and Sell Stock IV
---
+++ src/S0175CombineTwoTables.sql
@@ -0,0 +1,9 @@
+Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255))
+Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255))
+Truncate table Person
+insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen')
+Truncate table Address
+insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York')
+
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person LEFT OUTER JOIN Address ON Person.PersonId = Address.PersonId;
