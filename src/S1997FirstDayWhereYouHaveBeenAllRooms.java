23ce4ce22c40c5cccfe961ca16690fec6dfddc9c
camelcc <camel.young@gmail.com>
2021-09-10 14:44:47 -0700

    9/10/2021

---
+++ src/S1997FirstDayWhereYouHaveBeenAllRooms.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/first-day-where-you-have-been-in-all-the-roo
+public class S1997FirstDayWhereYouHaveBeenAllRooms {
+    public int firstDayBeenInAllRooms(int[] nextVisit) {
+        int rooms = nextVisit.length;
+        long dp[] = new long[rooms];
+        int mod = (int)(1e9)+7;
+        for (int i=1 ; i<rooms ; i++) {
+            if (nextVisit[i-1] == i-1)
+                dp[i] = dp[i-1]+2;
+            else
+                dp[i] = (dp[i-1] + dp[i-1] - dp[nextVisit[i-1]] + 2 + mod)%mod;
+        }
+        return (int)dp[rooms-1];
+    }
+}
